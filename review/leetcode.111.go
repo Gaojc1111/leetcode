03b2dc8041b7462d567db20c902d37d3dcb6bcf3
GaoJinCheng <gaojc1111@gmail.com>
2025-04-20 23:22:28 +0800

    2025.4.20

---
+++ review/leetcode.111.go
@@ -0,0 +1,19 @@
+package tree
+
+func minDepth(root *TreeNode) int {
+	if root == nil {
+		return 0
+	}
+	l := minDepth(root.Left)
+	r := minDepth(root.Right)
+	if l == 0 && r == 0 {
+		return 1
+	}
+	if l == 0 {
+		return r + 1
+	}
+	if r == 0 {
+		return l + 1
+	}
+	return min(l, r) + 1
+}
